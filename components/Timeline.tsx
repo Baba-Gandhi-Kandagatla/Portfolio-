28d7a8027f79c4a7ea00c2e5822cd8490d3f40e7
Baba-Gandhi-Kandagatla <babagandhikandagatla@gmail.com>
2025-01-15 00:08:51 +0530

    Update Timeline.tsx

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useState, forwardRef } from 'react'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+@typescript-eslint/no-unused-vars
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { ImageIcon } from 'lucide-react'
