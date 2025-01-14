ca7460eef25fc992a3d51729cb526fa2c57e051e
Baba-Gandhi-Kandagatla <babagandhikandagatla@gmail.com>
2025-01-15 00:09:15 +0530

    Update Timeline.tsx

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState, forwardRef } from 'react'
-@typescript-eslint/no-unused-vars
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
